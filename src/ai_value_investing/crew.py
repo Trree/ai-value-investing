5f6cbce0bb9addaf8c6e51223db60f37401f6a2c
Trree <wufeilongsky@outlook.com>
2025-07-31 18:08:34 +0800

    add gradio

--- src/ai_value_investing/crew.py
+++ src/ai_value_investing/crew.py
@@ -109,6 +109,7 @@ class AiValueInvesting():
             tasks=self.tasks, # Automatically created by the @task decorator
             process=Process.sequential,
             verbose=True,
+            memory=True,
             output_log_file=True,
             # process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
         )
